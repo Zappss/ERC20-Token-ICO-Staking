d5e571383cb720e61dcac0ae8ef3c4f80c87461d
Matthew C. Pilsbury <Matt@Matthew-Pilsburys-MacBook-Pro-2.local>
2020-08-01 19:45:25 -0400

    Deployed to Ropsten Token - 0x93903333a4162885f43cd186aefefc246ffa927a ICO - 0x27DB5Fa7F9C5d9a2f9260316db4C004aEf7CBEfa

--- yourtoken.sol
+++ yourtoken.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 abstract contract Context {
     function _msgSender() internal view virtual returns (address payable) {
